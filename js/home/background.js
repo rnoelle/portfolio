f61d5dac992712e10cf5fa6f24286cadac141dfa
rnoelle <reidnoelle2@gmail.com>
2016-11-28 11:09:52 -0700

    smoothed animation, changed vh to px

--- js/home/background.js
+++ js/home/background.js
@@ -5,26 +5,25 @@ angular.module('portfolio')
       restrict: 'E',
       link: function (scope, element, attrs) {
         var context = document.getElementById('back-canvas').getContext('2d');
+        var canvas = document.getElementById('back-canvas');
         var img = new Image();
+        img.src = 'assets/splitPyramid.png';
 
         img.onload = function () {
           context.drawImage(img, 60, 60, 650, 1200, 0, 0, 600, 600)
         }
 
-        img.src = 'assets/splitPyramid.png';
 
+        var winScroll = $(window).scrollTop()
         $(window).scroll(function () {
-          var winScroll = $(this).scrollTop();
-          // console.log('break');
-          // console.log('x', (440*(Math.floor($(window).scrollTop()/20))+60)-(4400*Math.floor(($(window).scrollTop()/20)/11)));
-          // console.log('y', 440*Math.floor((Math.floor($(window).scrollTop()/20))/11)+60);
+          winScroll = $(window).scrollTop()
+
           context.drawImage(img,
               (440*(Math.floor(winScroll/40))+60)-(4400*Math.floor((winScroll/40)/11)),
               440*Math.floor((Math.floor(winScroll/40))/11)+60,
               650, 1200, 0, 0, 600, 600);
 
           $('.background-overlay').css("opacity", (winScroll/4000)+.35)
-          console.log(winScroll);
           if (winScroll <= 2562) {
             $('#title').css({
               'transform': 'translate(0px, '+ winScroll/1.51 +'px)'
@@ -33,7 +32,7 @@ angular.module('portfolio')
             $('.scroll-nav-container').addClass('hidden-nav-cover');
             $('.original-scroll').css({'opacity': '0'});
           }
-          if (winScroll >= 3476) {
+          if (winScroll >= 3390) {
             $('body').css({'overflow': 'hidden'});
           }
         })
